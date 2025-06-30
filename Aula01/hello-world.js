7cefd7a2509df401f676461111da988d8e831587
Joel Santos <joeloliveira56@gmail.com>
2025-06-30 20:52:49 -0300

    :sparkles: feat: Criação dos arquivos da Aula 01 - Hello World e Tipos de variaveis

---
+++ Aula01/hello-world.js
@@ -0,0 +1,17 @@
+console.log("Hello World!"); // Primeiro código em JavaScript
+
+// Tipos de Variaveis 
+
+let nome = "João"; // Declaração de variável com let pode ser reatribuída
+const idade = 30; // Declaração de constante com const não pode ser reatribuída
+var cidade = "São Paulo"; // Declaração de variável com var
+console.log(nome); // Saída: João
+console.log(idade); // Saída: 30
+console.log(cidade); // Saída: São Paulo
+
+//mudando os dados
+nome = "Maria"; // Reatribuindo valor a variável nome
+console.log(nome); // Saída: Maria
+
+//idade = 31; // Isso causará um erro, pois idade é uma constante
+//console.log(idade); // Descomente para ver o erro
